cd326670b4a7a0556e43e55d1403b6ec3601e82e
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-02 13:37:40 +0000

    Number guessing game

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+import java.util.Random;
 /**
- *
- * @author 
+ *number guessing game
+ * @author  Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   //make a Scanner
+   Scanner input = new Scanner(System.in);
+   //create random number generator
+   Random rand = new Random();
+
+   //create a number number
+   int randNum = rand.nextInt(100) + 1;
+
+   //declare the guess variable
+   int guess;
+   //game loop
+   do{
+   //ask user for their guess
+   System.out.println("Please guess a number between 1 and 100");
+   guess = input.nextInt();
+   //is it too high or too low
+   if(guess > randNum){
+    System.out.println("Too High"); 
+   }else if(guess < randNum){
+    System.out.println("Too Low");
+     
+   }
+   }while(guess != randNum);
+   //outside the loop means that the user won
+   System.out.println("Congradulations! You Win");
+  
   }
 }
